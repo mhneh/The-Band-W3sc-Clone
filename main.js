3dd28247abdcf27ec2d675c65bd5a9e318a952a1
nh-zuy <hebiitachi2@gmail.com>
2022-10-16 14:56:34 +0700

    add slider and js for web

---
+++ main.js
@@ -0,0 +1,48 @@
+const buyButtons = document.getElementsByClassName("buy-button");
+var isOpenTicket = false;
+for (let btn of buyButtons) {
+    btn.addEventListener("click", showTicketModal);
+
+}
+
+function showTicketModal() {
+    document.getElementById("ticket-modal").style.display = "block";
+    document.getElementById("cover-layer").style.display = "block";
+    closeTicketModal();
+
+}
+function closeTicketModal() {
+    const closeButtons = document.getElementsByClassName("close");
+    for (let btn of closeButtons) {
+        btn.addEventListener("click", function (event) {
+            document.getElementById("ticket-modal").style.display = "none";
+            document.getElementById("cover-layer").style.display = "none";
+        });
+    }
+     // close ticket moda when click outside this
+    const coverLayer = document.getElementById("cover-layer");
+    coverLayer.addEventListener('click', function (event) {
+        document.getElementById("ticket-modal").style.display="none";
+        coverLayer.style.display = "none";
+        // else {
+        //     ticketModal.forEach(f => !f.contains(event.target) ? removeAttribute('open') : '');
+        // }
+    })
+
+}
+// set time out for slider
+var i = 0;
+function setSlider() {
+    var slides = document.getElementsByClassName("my-slide");
+    for (let slide of slides)
+        slide.style.display = "none";
+    if (i === slides.length)
+        i = 0;
+    slides[i].style.display = "block";
+    i++;
+    setTimeout(setSlider, 3000);
+
+}
+setSlider();
+
+
